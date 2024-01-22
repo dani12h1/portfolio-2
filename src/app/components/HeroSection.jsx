ac69d85409538b4c1647f50f18b38886afe7b21c
Daniel Nygaard <dani12h1@stud.kea.dk>
2024-01-22 13:11:42 +0100

    Navbar done

---
+++ src/app/components/HeroSection.jsx
@@ -0,0 +1,9 @@
+function HeroSection() {
+  return (
+    <section className="flex justify-center text-center h-full w-full top-20">
+      <h1 className="text-2xl font-medium">Fuuuuck af</h1>
+    </section>
+  );
+}
+
+export default HeroSection;
---
+++ src/app/components/navbar.jsx
@@ -0,0 +1,60 @@
+"use client";
+
+import React, { useState, useEffect } from "react";
+import { HiMenu } from "react-icons/hi";
+import HeroSection from "../components/HeroSection";
+import Link from "next/link";
+
+const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isSticky, setIsSticky] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <>
+      <header className="w-full fixed top-0 left-0 right-0 mx-auto">
+        <nav className={`py-4 md:px-12 px-4 bg-white ${isSticky ? "sticky top-0 right-0 left-0 bg-white " : ""}`}>
+          <div className="flex items-center justify-between">
+            <div className="text-black font-bold text-lg cursor-pointer">
+              {/* <Image src={logo} alt="" className="h-10" /> */}
+              <h2>LOGO</h2>
+            </div>
+
+            {/* for larger device */}
+            <div className="lg:flex items-center gap-3 hidden text-body">
+              <a className="block text-black  hover:text-gray-400 py-2 px-4 cursor-pointer">About me</a>
+              <a href="https://www.linkedin.com/in/danielnygaardc/" className="block py-2 px-4 cursor-pointer text-black">
+                Linkedin
+              </a>
+            </div>
+
+            {/* contact me btn */}
+            <div className="lg:block hidden">
+              <button className="px-4 py-2 bg-[#6366F1] hover:bg-[#6366F4] border text-white rounded hover:bg-primary hover:text-black transition-all duration-300">Theme</button>
+            </div>
+
+            {/* btn for small devices */}
+            <button onClick={toggleMenu} className="lg:hidden text-black text-3xl">
+              <HiMenu />
+            </button>
+          </div>
+
+          {/* Mobile menu */}
+          {isMenuOpen && (
+            <div className="mt-4 bg-body p-4 rounded-lg text-black">
+              <a className="block py-2 px-4 cursor-pointer text-black">About me</a>
+              <a href="https://www.linkedin.com/in/danielnygaardc/" className="block py-2 px-4 cursor-pointer text-black">
+                Linkedin
+              </a>
+            </div>
+          )}
+        </nav>
+      </header>
+    </>
+  );
+};
+
+export default Navbar;
