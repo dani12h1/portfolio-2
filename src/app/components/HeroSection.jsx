71b525e3d8e43b9836b9e4de2011d4d3d1f0d55d
Daniel Nygaard <dani12h1@stud.kea.dk>
2024-01-23 14:26:05 +0100

    forside button virker ikke

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -7,18 +7,18 @@ import React from "react";
 function HeroSection() {
   return (
     <>
-      <motion.section initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ ease: "easeInOut", duration: 0.9, delay: 0.2 }} className="flex h-screen bg-hvid text-black container mx-auto">
-        <div className=" flex flex-col justify-center items-start lg:w-1/2 p-8">
-          <h1 className="text-black font-bold lg:text-4xl text:1xl">
-            DEVELOPER & <br></br> MULTIMEDIA DESIGNER
-          </h1>
-          <div className="pt-6 text-xl">
-            <button className="px-4 py-2 bg-[#6366F1] hover:bg-[#6366F4] border text-white rounded hover:bg-primary hover:text-black transition-all duration-300">Contact</button>
-          </div>
+      <motion.section initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ ease: "easeInOut", duration: 0.9, delay: 0.2 }} className="flex flex-col lg:flex-row h-screen bg-hvid text-black container mx-auto mt-16 lg:mt-0">
+        <div className="flex justify-center items-center lg:w-1/2 p-8 order-2 lg:order-1">
+          <motion.h1 initial={{ opacity: 0, y: -180 }} animate={{ opacity: 1, y: 0 }} transition={{ ease: "easeInOut", duration: 0.9, delay: 0.2 }} className="text-black font-bold lg:text-4xl text-2xl text-center lg:text-left">
+            DEVELOPER & <br></br> MULTIMEDIADESIGNER
+          </motion.h1>
         </div>
-        <motion.div initial={{ opacity: 0, y: -180 }} animate={{ opacity: 1, y: 0 }} transition={{ ease: "easeInOut", duration: 0.9, delay: 0.2 }} className="flex justify-center items-center lg:w-1/2 p-8">
+        <div className="flex justify-center items-center lg:w-1/2 p-8 order-1 lg:order-3">
           <Image src={Mig} width={500} height={500} alt="mig" />
-        </motion.div>
+        </div>
+        <div className="text-center lg:pt-6 order-3 lg:order-2 ">
+          <button className="px-4 py-2 bg-[#6366F1] hover:bg-[#6366F4] border text-white rounded hover:bg-primary hover:text-black transition-all duration-300">Contact</button>
+        </div>
       </motion.section>
     </>
   );
