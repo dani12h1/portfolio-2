e50a32e4ee6ac2cc642556955afc0f0ae16a7cc8
Daniel Nygaard <dani12h1@stud.kea.dk>
2024-01-24 14:58:22 +0100

    hey

--- src/app/projects/[id]/page.jsx
+++ src/app/projects/[id]/page.jsx
@@ -18,26 +18,24 @@ export default function ProjectList({ params }) {
     <>
       <body>
         <Navbar />
-        <div className="flex container mx-auto pt-32">
-          {/* <h1>{title}</h1> */}
-          <section>
-            <p className="font-bold pb-4">{content.heading}</p>
-            <div className="flex flex-col gap-6 pb-14">
+        <div className="flex container mx-auto pt-36">
+          <section className="">
+            <h1 className="pb-4">{content.heading}</h1>
+            <div className="flex flex-col gap-6 pb-14 lg:flex-row lg:mx-auto">
               <Image className="rounded-lg" src={placeholder} alt="placeholder"></Image>
               <Image className="rounded-lg" src={placeholder} alt="placeholder"></Image>
               <Image className="rounded-lg" src={placeholder} alt="placeholder"></Image>
             </div>
-            <h3 className="font-bold">Website:</h3>
+
+            <h3>Website:</h3>
             <p className="pb-4 ">{content.website}</p>
-            <h3 className="font-bold">Tools & Technologies</h3>
+            <h3>Tools & Technologies</h3>
             <ul className="pb-4">
               {content.technologies[0].techs.map((tech, index) => (
-                <li className="" key={index}>
-                  {tech}
-                </li>
+                <li key={index}>{tech}</li>
               ))}
             </ul>
-            <h3 className="font-bold">Challenge</h3>
+            <h3>Challenge</h3>
             <p className="pb-10">{content.text}</p>
           </section>
         </div>
