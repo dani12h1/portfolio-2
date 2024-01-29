e05f458fcf4662ff23b55572a7bbe791168774a7
Daniel Nygaard <dani12h1@stud.kea.dk>
2024-01-29 13:36:22 +0100

    hero

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Head from "next/head";
 import Navbar from "../components/navbar";
 import Image from "next/image";
-import logo from "../assets/logo.png";
+import me from "../assets/me.png";
 import Footer from "../components/Footer";
 
 function about() {
@@ -13,8 +13,8 @@ function about() {
       </Head>
       <Navbar />
       <section className="mt-72 container mx-auto">
-        <div className="lg:flex gap-44 justify-center ">
-          <Image className="pb-10" src={logo} alt="" width={200} height={200} />
+        <div className="lg:flex gap-36 justify-center ">
+          <Image className="pb-10" src={me} alt="" width={250} height={200} />
           <p>
             Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil vel illum asperiores dignissimos cumque quibusdam et fugiat voluptatem nobis suscipit explicabo, eaque consequatur nesciunt, fugit eligendi corporis laudantium adipisci soluta? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt totam dolorum, ducimus obcaecati, voluptas facilis molestias nobis ut quam natus similique inventore excepturi optio ipsa deleniti fugit illo. Unde, amet! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum illo necessitatibus perspiciatis! Aperiam perferendis labore temporibus, eos culpa corporis recusandae quas, fuga voluptatibus nesciunt odit libero tenetur neque consequatur ea. Lorem ipsum dolor, sit amet consectetur adipisicing elit.<br></br>
             <br></br>Nihil vel illum asperiores dignissimos cumque quibusdam et fugiat voluptatem nobis suscipit explicabo, eaque consequatur nesciunt, fugit eligendi corporis laudantium adipisci soluta?
--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -7,21 +7,20 @@ import React from "react";
 function HeroSection() {
   return (
     <>
-      <motion.section initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ ease: "easeInOut", duration: 0.9, delay: 0.2 }} className="flex flex-col lg:flex-row h-screen bg-hvid text-black container mx-auto mt-16 lg:mt-0">
+      <section className="flex flex-col lg:flex-row h-screen bg-hvid text-black container mx-auto mt-16 lg:mt-0">
         <div className="lg:flex lg:flex-col block justify-center text-center   p-8 order-2 lg:order-1 ">
-          <h2 className="text-7xl">Hello! I’m Daniel</h2>
+          <motion.h2 initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ ease: "easeInOut", duration: 0.9, delay: 0.2 }} className="text-7xl">
+            Hello! I’m Daniel
+          </motion.h2>
           <motion.h1 initial={{ opacity: 0, y: -180 }} animate={{ opacity: 1, y: 0 }} transition={{ ease: "easeInOut", duration: 0.9, delay: 0.2 }} className="text-black font-thin  lg:text-4xl text-2xl lg:text-left">
             developer and multimediadesigner
           </motion.h1>
 
           <div className=" lg:pt-6 lg:order-2 text-center lg:text-left pt-6">
-            <button className="px-4 py-2 bg-[#6366F1] hover:bg-[#6366F4] border text-white rounded hover:bg-primary hover:text-black transition-all duration-300 ">Contact</button>
+            <button className="px-4 py-2 bg-[#6366F1] hover:bg-[#5256c8] border text-white text-2xl rounded hover:bg-primary transition-all duration-300 ">Contact</button>
           </div>
         </div>
-        {/* <div className="flex justify-center items-center lg:w-1/2 p-8 order-1 lg:order-3">
-          <Image src={Mig} width={500} height={500} alt="mig" />
-        </div> */}
-      </motion.section>
+      </section>
     </>
   );
 }
