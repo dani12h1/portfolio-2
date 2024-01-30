9b1703b6b700155dd2ec462e862bdbd542fe232d
Daniel Nygaard <dani12h1@stud.kea.dk>
2024-01-30 23:28:53 +0100

    font

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -8,17 +8,17 @@ function HeroSection() {
   return (
     <>
       <section className="flex flex-col lg:flex-row h-screen bg-hvid text-black container mx-auto mt-16 lg:mt-0">
-        <div className="lg:flex lg:flex-col block justify-center text-center   p-8 order-2 lg:order-1 ">
-          <motion.h2 initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ ease: "easeInOut", duration: 0.9, delay: 0.2 }} className="text-7xl">
+        <div className="lg:flex lg:flex-col block justify-center   p-8 order-2 lg:order-1 ">
+          <motion.h1 initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ ease: "easeInOut", duration: 0.9, delay: 0.2 }} className="text-7xl font-extrabold">
             Hello! I’m Daniel
-          </motion.h2>
-          <motion.h1 initial={{ opacity: 0, y: -180 }} animate={{ opacity: 1, y: 0 }} transition={{ ease: "easeInOut", duration: 0.9, delay: 0.2 }} className="text-black font-thin  lg:text-4xl text-2xl lg:text-left">
-            developer and multimediadesigner
           </motion.h1>
+          <motion.h2 initial={{ opacity: 0, y: -180 }} animate={{ opacity: 1, y: 0 }} transition={{ ease: "easeInOut", duration: 0.9, delay: 0.2 }} className="text-black font-thin  lg:text-6xl text-2xl lg:text-left">
+            a developer and multimediadesigner
+          </motion.h2>
 
-          <div className=" lg:pt-6 lg:order-2 text-center lg:text-left pt-6">
+          {/* <div className=" lg:pt-6 lg:order-2 text-center lg:text-left pt-6">
             <button className="px-4 py-2 bg-[#6366F1] hover:bg-[#5256c8] border text-white text-2xl rounded hover:bg-primary transition-all duration-300 ">Contact</button>
-          </div>
+          </div> */}
         </div>
       </section>
     </>
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,12 @@
-import { Inter } from "next/font/google";
+import { Roboto } from "next/font/google";
 import "./globals.css";
 import Head from "next/head";
 import Navbar from "./components/navbar";
 
-const inter = Inter({ subsets: ["latin"] });
+const roboto = Roboto({
+  weight: "300",
+  subsets: ["latin"],
+});
 
 export const metadata = {
   title: "Portfolio",
@@ -14,7 +17,7 @@ export default function RootLayout({ children }) {
   return (
     <>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={roboto.className}>{children}</body>
       </html>
     </>
   );
