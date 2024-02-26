ce57a38e47397afb10b8344f1b2725e8cd1ccb32
Daniel Nygaard <dani12h1@stud.kea.dk>
2024-02-26 13:29:28 +0100

    test text done

--- src/app/components/test.jsx
+++ src/app/components/test.jsx
@@ -11,14 +11,14 @@ const TextAnimation = () => {
   };
 
   return (
-    <div className="flex items-center justify-center h-screen ">
-      <motion.div className="relative overflow-hidden pb-4" style={{ height: "50px", width: "1000px" }} onMouseEnter={handleHover} onMouseLeave={handleHover}>
-        <motion.div initial={{ y: "100%" }} animate={isHovered ? { y: -40 } : { y: "0%" }} transition={{ duration: 0.3 }} className="absolute inset-0 flex items-center justify-center text-4xl cursor-pointer z-10">
+    <div className="flex items-center justify-center ">
+      <motion.div className="relative overflow-hidden" style={{ height: "15px", width: "215px" }} onMouseEnter={handleHover} onMouseLeave={handleHover}>
+        <motion.p initial={{ y: "100%" }} animate={isHovered ? { y: -35 } : { y: "0%" }} transition={{ duration: 0.3 }} className="absolute inset-0 flex items-center justify-center  cursor-pointer z-10 text-sm">
           1 / HOGWARTS STUDENT LIST
-        </motion.div>
-        <motion.div initial={{ y: "100%" }} animate={isHovered ? { y: 0 } : { y: "100%" }} transition={{ duration: 0.3 }} className="absolute inset-0 flex items-center justify-center text-4xl cursor-pointer z-0">
+        </motion.p>
+        <motion.p initial={{ y: "100%" }} animate={isHovered ? { y: 0 } : { y: "100%" }} transition={{ duration: 0.3 }} className="absolute inset-0 flex items-center justify-center  cursor-pointer z-0 text-sm">
           VIEW PROJECT
-        </motion.div>
+        </motion.p>
       </motion.div>
     </div>
   );
